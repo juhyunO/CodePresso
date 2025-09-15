cf841068ad3b663cc1688c4eed6e52b6153c8b71
Seoyoung-Kyung <tjdud90352@gmail.com>
2025-09-15 11:01:09 +0900

    initial commit

---
+++ src/main/java/com/codepresso/codepresso/entity/User.java
@@ -0,0 +1,31 @@
+package com.codepresso.codepresso.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Long email;
+
+    @Column
+    private String password;
+
+
+}
